d7b7256e342f59e62e14622614a13b29e3c53b19
Evan Suma Rosenberg <suma@umn.edu>
2022-11-03 16:58:31 -0500

    class

--- src/KeyframeAnimation.ts
+++ src/KeyframeAnimation.ts
@@ -14,12 +14,18 @@ export class KeyframeAnimation
     public targetObject: gfx.Transform3;
     public animationSpeed: number;
     public motionMode: string;
+
+    public keyframeTimes: number[];
+    public linearPath: gfx.LinearPath3;
     
     constructor(targetObject: gfx.Transform3, animationSpeed: number)
     {
         this.targetObject = targetObject;
         this.animationSpeed = animationSpeed;
         this.motionMode = 'Linear';
+
+        this.keyframeTimes = [];
+        this.linearPath = new gfx.LinearPath3();
     }
 
     isPlaying(): boolean
@@ -30,7 +36,27 @@ export class KeyframeAnimation
 
     addKeyframe(): void
     {
-        // TO BE ADDED
+        const index = this.keyframeTimes.length;
+
+        if(index == 0)
+        {
+            this.keyframeTimes.push(0);
+            this.linearPath.controlPoints.push(this.targetObject.position.clone());
+
+            console.log('Added keyframe ' + (index + 1) + '.');
+        }
+        else if(this.linearPath.controlPoints[index - 1].equals(this.targetObject.position))
+        {
+            console.log('Duplicate keyframe not added.');
+        }
+        else
+        {
+            const frameTime = this.keyframeTimes[index - 1] + this.animationSpeed;
+            this.keyframeTimes.push(frameTime);
+            this.linearPath.controlPoints.push(this.targetObject.position.clone());
+
+            console.log('Added keyframe ' + (index + 1) + '.');
+        }
     }
 
     play(): void
